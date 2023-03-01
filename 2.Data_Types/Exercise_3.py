4a9c331f2cde4742c007aa9b8195eec460557ecf
Stan <dimitrov.s.dev@gmail.com>
2023-03-01 09:42:40 +0200

    100_Days_of_Code

--- 2.Data_Types/Exercise_3.py
+++ 2.Data_Types/Exercise_3.py
@@ -4,3 +4,7 @@ age = input("What is your current age? ")
 
 # Write your code below this line 👇
 
+days = (90 - int(age)) * 365
+week = (90 - int(age)) * 52
+month = (90 - int(age)) * 12
+print(f"You have {days} days, {week} weeks, and {month} months left.")
